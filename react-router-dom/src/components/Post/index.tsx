3e742167ee308534c0addc7a74a507d01aa23617
Gabriel Cacharo <57328274+Gabriel-Cacharo@users.noreply.github.com>
2023-03-14 12:11:56 -0300

    :sparkles: Routes, Page not found and Outlet
    Routes, Page not found and Outlet

---
+++ react-router-dom/src/components/Post/index.tsx
@@ -0,0 +1,13 @@
+export const Post = () => {
+  return (
+    <div>
+      <h1>Post</h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates,
+        sint illo! Necessitatibus corporis facilis unde nostrum enim doloremque
+        nihil saepe, blanditiis id veritatis debitis delectus ex excepturi eius,
+        accusantium qui.
+      </h1>
+    </div>
+  );
+};
--- react-router-dom/src/main.tsx
+++ react-router-dom/src/main.tsx
@@ -4,15 +4,32 @@ import ReactDOM from "react-dom/client";
 
 import { Home } from "./components/Home";
 import { About } from "./components/About";
+import { Posts } from "./components/Posts";
+import { Redirect } from "./components/Redirect";
+import { Menu } from "./components/Menu";
+import { Post } from "./components/Post";
+import { NotFound } from "./components/NotFound";
 
 import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <Menu />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/post" element={<Posts />} />
+        {/* <Route path="/post/:id" element={<Posts />} /> */}
+
+        <Route path="/post" element={<Posts />}>
+          <Route path=":id" element={<Post />} />
+        </Route>
+
+        <Route path="/redirect" element={<Redirect />} />
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
