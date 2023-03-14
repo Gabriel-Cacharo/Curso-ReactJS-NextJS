9bf00df1782f06f92150dc79d8e17a9bd3fb868a
Gabriel Cacharo <57328274+Gabriel-Cacharo@users.noreply.github.com>
2023-03-14 12:10:44 -0300

    :sparkles: Redirect ( navigate )
    Redirect ( navigate )

---
+++ react-router-dom/src/components/Redirect/index.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const Redirect = () => {
+  const [time, setTime] = useState(3);
+  const timeout = useRef(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    clearTimeout(timeout.current);
+
+    timeout.current = setTimeout(() => {
+      setTime((t) => t - 1);
+    }, 1000);
+
+    if (time <= 0) {
+      navigate("/about", {
+        state: `This is the state from Redirect < ${Math.random()} >`,
+      });
+    }
+
+    return () => {
+      clearTimeout(timeout.current);
+    };
+  }, [time]);
+
+  return (
+    <div>
+      <h1>Get out of here in: {time}</h1>
+    </div>
+  );
+};
